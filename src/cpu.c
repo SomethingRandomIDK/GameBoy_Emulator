4a9694c294c8b6972dae46614d1c7858ff95ddb0
Mehul <mehtomar@gmail.com>
2024-08-21 21:58:42 -0400

    Didn't want to type too many capital letters

--- src/cpu.c
+++ src/cpu.c
@@ -1,41 +1,41 @@
 #include "./include/cpu.h"
 
-cpu_t gbCPU;
+cpu_t gbcpu;
 
 uint16_t regAF() {
     uint16_t ret;
-    ret = (gbCPU.regs.a << 8) | gbCPU.regs.f;
+    ret = (gbcpu.regs.a << 8) | gbcpu.regs.f;
     return ret;
 }
 
 uint16_t regBC() {
     uint16_t ret;
-    ret = (gbCPU.regs.b << 8) | gbCPU.regs.c;
+    ret = (gbcpu.regs.b << 8) | gbcpu.regs.c;
     return ret;
 }
 
 uint16_t regDE() {
     uint16_t ret;
-    ret = (gbCPU.regs.d << 8) | gbCPU.regs.e;
+    ret = (gbcpu.regs.d << 8) | gbcpu.regs.e;
     return ret;
 }
 
 uint16_t regHL() {
     uint16_t ret;
-    ret = (gbCPU.regs.h << 8) | gbCPU.regs.l;
+    ret = (gbcpu.regs.h << 8) | gbcpu.regs.l;
     return ret;
 }
 
 void initCPU() {
-    gbCPU.regs.a = 0xaa;
-    gbCPU.regs.b = 0xbb;
-    gbCPU.regs.c = 0xcc;
-    gbCPU.regs.d = 0xdd;
-    gbCPU.regs.e = 0xee;
-    gbCPU.regs.h = 0x11;
-    gbCPU.regs.l = 0x22;
-    gbCPU.regs.f = 0xff;
-    gbCPU.regs.sp = 0;
-    gbCPU.regs.pc = 0;
+    gbcpu.regs.a = 0xaa;
+    gbcpu.regs.b = 0xbb;
+    gbcpu.regs.c = 0xcc;
+    gbcpu.regs.d = 0xdd;
+    gbcpu.regs.e = 0xee;
+    gbcpu.regs.h = 0x11;
+    gbcpu.regs.l = 0x22;
+    gbcpu.regs.f = 0xff;
+    gbcpu.regs.sp = 0;
+    gbcpu.regs.pc = 0;
 }
 
