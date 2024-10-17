e9c500b65351bb6cc8997ac3078100c4d104a457
Mehul <mehtomar@gmail.com>
2024-10-17 01:11:49 -0400

    Implemented Rom Reading and Rom Writing functions and added to bus

--- src/bus.c
+++ src/bus.c
@@ -1,84 +1,85 @@
+#include "./include/cart.h"
 #include "./include/bus.h"
 #include "./include/cpu.h"
 
 uint8_t busRead8(uint16_t addr) {
     if (addr < 0x8000) {
-	// Read from cart
-	return 0;
+        // Read from cart
+        return romRead(addr);
     } else if (addr < 0xa000) {
-	// Read from vram
-	return 0;
+        // Read from vram
+        return 0;
     } else if (addr < 0xc000) {
-	// Read from cart (RAM)
-	return 0;
+        // Read from cart (RAM)
+        return romRead(addr);
     } else if (addr < 0xe000) {
-	// read from WRAM
-	return wramRead(addr);
+        // read from WRAM
+        return wramRead(addr);
     } else if (addr < 0xfe00) {
-	// Echo Ram but it wraps around so reading or writing from here should
-	// do the same thing as reading and writing to 0xc000 to 0xddff
-	return wramRead(addr - 0x2000);
+        // Echo Ram but it wraps around so reading or writing from here should
+        // do the same thing as reading and writing to 0xc000 to 0xddff
+        return wramRead(addr - 0x2000);
     } else if (addr < 0xfea0) {
-	// OAM (Object attribute memory) I think this is where the sprites are
-	// saved
-	return 0;
+        // OAM (Object attribute memory) I think this is where the sprites are
+        // saved
+        return 0;
     } else if (addr < 0xff00) {
-	// Nintendo says that using this space is prohibited, but there is still
-	// some stuff that happens when you do use it
-	// When the OAM is blocked it just returns 0xff
-	// Due to a bug it is possible to trigger OAM corruption (NEED to do 
-	// more research)
-	return 0;
+        // Nintendo says that using this space is prohibited, but there is still
+        // some stuff that happens when you do use it
+        // When the OAM is blocked it just returns 0xff
+        // Due to a bug it is possible to trigger OAM corruption (NEED to do 
+        // more research)
+        return 0;
     } else if (addr < 0xff80) {
-	// IO Registers
-	return 0;
+        // IO Registers
+        return 0;
     } else if (addr < 0xffff) {
-	// HRAM
-	return hramRead(addr);
+        // HRAM
+        return hramRead(addr);
     } else {
-	// Interupt register
-	return 0;
+        // Interupt register
+        return 0;
     }
     return 0;
 }
 
 void busWrite8(uint16_t addr, uint8_t val){
     if (addr < 0x8000) {
-	// Write from cart
-	return;
+        // Write from cart
+        romWrite(addr, val);
     } else if (addr < 0xa000) {
-	// Write from vram
-	return;
+        // Write from vram
+        return;
     } else if (addr < 0xc000) {
-	// Write from cart (RAM)
-	return;
+        // Write from cart (RAM)
+        romWrite(addr, val);
     } else if (addr < 0xe000) {
-	// Write from WRAM
-	wramWrite(addr, val);
+        // Write from WRAM
+        wramWrite(addr, val);
     } else if (addr < 0xfe00) {
-	// Echo Ram but it wraps around so reading or writing from here should
-	// do the same thing as reading and writing to 0xc000 to 0xddff
-	wramWrite(addr - 0x2000, val);
+        // Echo Ram but it wraps around so reading or writing from here should
+        // do the same thing as reading and writing to 0xc000 to 0xddff
+        wramWrite(addr - 0x2000, val);
     } else if (addr < 0xfea0) {
-	// OAM (Object attribute memory) I think this is where the sprites are
-	// saved
-	return;
-     } else if (addr < 0xff00) {
-	// Nintendo says that using this space is prohibited, but there is still
-	// some stuff that happens when you do use it
-	// When the OAM is blocked it just returns 0xff
-	// Due to a bug it is possible to trigger OAM corruption (NEED to do 
-	// more research)
-	return;
+        // OAM (Object attribute memory) I think this is where the sprites are
+        // saved
+        return;
+    } else if (addr < 0xff00) {
+        // Nintendo says that using this space is prohibited, but there is still
+        // some stuff that happens when you do use it
+        // When the OAM is blocked it just returns 0xff
+        // Due to a bug it is possible to trigger OAM corruption (NEED to do 
+        // more research)
+        return;
     } else if (addr < 0xff80) {
-	// IO Registers
-	return;
+        // IO Registers
+        return;
     } else if (addr < 0xffff) {
-	// HRAM
-	hramWrite(addr, val);
+        // HRAM
+        hramWrite(addr, val);
     } else {
-	// Interupt register
-	return;
+        // Interupt register
+        return;
     }
     return;
 }
--- src/cart.c
+++ src/cart.c
@@ -3,7 +3,7 @@
 
 #include "./include/cart.h"
 
-rom_t rom;
+static rom_t rom;
 
 static const char *newLicenseeText() {
     switch(rom.header.newLicenseeCode) {
@@ -961,6 +961,50 @@ static void mapperMBC5Write(uint16_t addr, uint8_t val) {
 // MBC6 WILL NOT BE SUPPORTED
 // MBC7 WILL NOT BE SUPPORTED
 // MMM01 MIGHT BE SUPPORTED IN THE FUTURE
+
+memMapRead_t mapperReadFuncs[] = {
+    [ROM] = &mapperRomRead,
+    [MBC1] = &mapperMBC1Read,
+    [MBC2] = &mapperMBC2Read,
+    [MBC3] = &mapperMBC3Read,
+    [MBC5] = &mapperMBC5Read,
+    [MBC6] = NULL,
+    [MBC7] = NULL,
+    [MMM01] = NULL,
+    [HUC1] = NULL,
+    [HUC3] = NULL
+};
+
+memMapWrite_t mapperWriteFuncs[] = {
+    [ROM] = &mapperRomWrite,
+    [MBC1] = &mapperMBC1Write,
+    [MBC2] = &mapperMBC2Write,
+    [MBC3] = &mapperMBC3Write,
+    [MBC5] = &mapperMBC5Write,
+    [MBC6] = NULL,
+    [MBC7] = NULL,
+    [MMM01] = NULL,
+    [HUC1] = NULL,
+    [HUC3] = NULL
+};
+
+uint8_t romRead(uint16_t addr) {
+    if (!mapperReadFuncs[rom.cType]) {
+        printf("Cartridge type not supported \n");
+        exit(-1);
+    }
+
+    return mapperReadFuncs[rom.cType](addr);
+}
+
+void romWrite(uint16_t addr, uint8_t val) {
+    if (!mapperWriteFuncs[rom.cType]) {
+        printf("Cartridge type not supported \n");
+        exit(-1);
+    }
+
+    mapperWriteFuncs[rom.cType](addr, val);
+}
 
 void cartInit(char *file) {
     rom.filenameSize = strlen(file);
