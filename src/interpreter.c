424753809b3cc69df35aaa10235258439327fab9
Mehul <mehtomar@gmail.com>
2024-10-21 12:56:42 -0400

    Added more information to logging messages, and added JP instruction

--- src/interpreter.c
+++ src/interpreter.c
@@ -8,12 +8,23 @@
 typedef void (*inst)(gb_t *);
 
 static void nop(gb_t *cpu) {
+    char msg[64];
+    sprintf(msg, "Inst: NOP  InstCode: 0x00 PC: %x", cpu->regs.pc); 
     cpu->regs.pc ++;
-    logMessage("Inst: NOP  InstCode: 0x00", TRACE);
+    logMessage(msg, TRACE);
+}
+
+static void jp(gb_t *cpu) {
+    char msg[64];
+    sprintf(msg, "Inst: JP  InstCode: 0xc3 PC: %x", cpu->regs.pc);
+    uint16_t addr = busRead16(++cpu->regs.pc);
+    cpu->regs.pc = addr;
+    logMessage(msg, TRACE);
 }
 
 static inst instructions[0x100] = {
-    [0x00] = &nop
+    [0x00] = &nop,
+    [0xc3] = &jp
 };
 
 void runInst(gb_t *cpu) {
@@ -22,7 +33,7 @@ void runInst(gb_t *cpu) {
         instructions[curInst](cpu);
     } else {
         char msg[64];
-        sprintf(msg, "Instruction not recognized INST: %x", curInst);
+        sprintf(msg, "Instruction not recognized INST: %x PC: %x", curInst, cpu->regs.pc);
         logMessage(msg, ERROR);
         exit(-1);
     }
