03698d7661d7762e32ef0b4dba81bdace9c71553
Mehul <mehtomar@gmail.com>
2024-10-23 09:28:52 -0400

    Changed how logging works for instructions

--- src/interpreter.c
+++ src/interpreter.c
@@ -10,21 +10,15 @@ typedef void (*inst)(gb_t *);
 // NOP
 
 static void nop(gb_t *cpu) {
-    char msg[64];
-    sprintf(msg, "Inst: NOP  InstCode: 0x00 PC: %x", cpu->regs.pc); 
     cpu->regs.pc ++;
-    logMessage(msg, TRACE);
 }
 
 // 8-BIT LD
 
 static void ld_nn_a(gb_t *cpu) {
-    char msg[64];
-    sprintf(msg, "Inst: LD nn A InstCode: 0xea PC: %x", cpu->regs.pc);
     uint16_t addr = busRead16(++cpu->regs.pc);
     cpu->regs.pc += 2;
     busWrite8(addr, cpu->regs.a);
-    logMessage(msg, TRACE);
 }
 
 // XOR
@@ -33,56 +27,63 @@ static void xor_a(gb_t *cpu) {
     cpu->regs.a ^= cpu->regs.a;
     cpu->regs.f |= 0x80;
     cpu->regs.f &= 0x8f;
-    char msg[64];
-    sprintf(msg, "Inst: XOR A  InstCode: 0xaf PC: %x A: %x F: %x", cpu->regs.pc, cpu->regs.a, cpu->regs.f);
     cpu->regs.pc++;
-    logMessage(msg, TRACE);
 }
 
 // INTERUPTS
 
 static void di(gb_t *cpu) {
-    char msg[64];
-    sprintf(msg, "Inst: DI InstCode: 0xf3 PC: %x", cpu->regs.pc);
     cpu->interrupts = false;
     cpu->regs.pc++;
-    logMessage(msg, TRACE);
 }
 
 // JUMPS
 
-static void jp(gb_t *cpu) {
-    char msg[64];
-    sprintf(msg, "Inst: JP  InstCode: 0xc3 PC: %x", cpu->regs.pc);
+static void jp_nn(gb_t *cpu) {
     uint16_t addr = busRead16(++cpu->regs.pc);
     cpu->regs.pc = addr;
-    logMessage(msg, TRACE);
 }
 
-static void jp_nz(gb_t *cpu) {
-    char msg[64];
-    sprintf(msg, "Inst: JP NZ  InstCode: 0xc2 PC: %x", cpu->regs.pc);
+static void jp_nz_nn(gb_t *cpu) {
     if ((cpu->regs.f & 0x80) == 0) {
         uint16_t addr = busRead16(++cpu->regs.pc);
         cpu->regs.pc = addr;
     } else {
         cpu->regs.pc += 3;
     }
-    logMessage(msg, TRACE);
 }
 
 static inst instructions[0x100] = {
     [0x00] = &nop,
     [0xaf] = &xor_a,
-    [0xc2] = &jp_nz,
-    [0xc3] = &jp,
+    [0xc2] = &jp_nz_nn,
+    [0xc3] = &jp_nn,
     [0xea] = &ld_nn_a,
     [0xf3] = &di
+};
+
+static char *instNames[0x100] = {
+    [0x00] = "NOP",
+    [0xaf] = "XOR A",
+    [0xc2] = "JP NZ nn",
+    [0xc3] = "JP nn",
+    [0xea] = "LD nn A",
+    [0xf3] = "DI"
 };
 
 void runInst(gb_t *cpu) {
     uint8_t opcode = busRead8(cpu->regs.pc);
     if (instructions[opcode]) {
+        if (instNames[opcode]) {
+            char msg[128];
+            sprintf(msg, "INST: %s OPCODE: %02x PC: %04x A: %02x B: %02x C: %02x D: %02x E: %02x H: %02x L: %02x F: %02x",
+                    instNames[opcode], opcode, cpu->regs.pc, cpu->regs.a,
+                    cpu->regs.b, cpu->regs.c, cpu->regs.d, cpu->regs.e,
+                    cpu->regs.h, cpu->regs.l, cpu->regs.f);
+            logMessage(msg, TRACE);
+        } else
+            logMessage("Instruction Information Not Found", WARNING);
+
         instructions[opcode](cpu);
     } else {
         char msg[64];
