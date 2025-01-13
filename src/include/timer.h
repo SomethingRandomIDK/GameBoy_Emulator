c9f3d89d1fce6c73a948b0925337acf98d8d36f4
mehul <mehtomar@gmail.com>
2025-01-12 23:15:54 -0500

    Basic setup for Timer Added function to increment the timers Need to add all the places where the timer is incremented

---
+++ src/include/timer.h
@@ -0,0 +1,9 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include <stdint.h>
+
+void incTimer(uint32_t cycles);
+
+#endif
+
---
+++ src/timer.c
@@ -0,0 +1,49 @@
+#include "./include/timer.h"
+#include "./include/cart.h"
+#include "./include/interrupt.h"
+
+#define CLOCK 4194304
+
+// Can't name something div because of the function in the stdlib.h
+static uint16_t divReg = 0xab00;
+static uint8_t tima = 0;
+static uint8_t tma = 0;
+static uint8_t tac = 0;
+static uint32_t mbc3Cycles = 0;
+
+void incTimer(uint32_t cycles) {
+    mbc3Cycles += cycles;
+
+    if(mbc3Cycles >= CLOCK) {
+        updateRtcRegs();
+        mbc3Cycles -= CLOCK;
+    }
+
+    uint16_t prevDiv = divReg;
+    divReg += cycles;
+
+    bool checkIncTIMA = false;
+    switch(tac & 0x3) {
+        case 0x0:
+            checkIncTIMA = ((prevDiv >> 9) & 1) ^ ((divReg >> 9) & 1);
+            break;
+        case 0x1:
+            checkIncTIMA = ((prevDiv >> 3) & 1) ^ ((divReg >> 3) & 1);
+            break;
+        case 0x2:
+            checkIncTIMA = ((prevDiv >> 5) & 1) ^ ((divReg >> 5) & 1);
+            break;
+        case 0x3:
+            checkIncTIMA = ((prevDiv >> 7) & 1) ^ ((divReg >> 7) & 1);
+            break;
+    }
+
+    if ((tac & 0x04) && checkIncTIMA) {
+        tima++;
+        if (!(tima)) {
+            tima = tma;
+            raiseInterrupt(TIMER);
+        }
+    }
+}
+
