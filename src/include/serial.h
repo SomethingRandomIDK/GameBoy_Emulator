ece7d1ed4c1ea49ad92287b22c8a324f251bd3ed
Mehul <mehtomar@gmail.com>
2025-01-13 12:01:14 -0500

    Basic setup for serial io Need to leave so just a quick commit in the middle

---
+++ src/include/serial.h
@@ -0,0 +1,9 @@
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+#include <stdint.h>
+
+void incSerialTimer(uint32_t cycles);
+
+#endif
+
---
+++ src/serial.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+#include "./include/serial.h"
+
+static bool tranfer = 0;
+static uint32_t serialCycles = 0;
+static uint8_t sb = 0;
+static uint8_t sc = 0;
+
+void incSerialTimer(uint32_t cycles) {
+}
+
