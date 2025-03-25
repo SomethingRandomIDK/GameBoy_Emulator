dcfe2d7a8c5b5b7bec5c237864702abf5d8d9b6f
mehul <mehtomar@gmail.com>
2025-03-25 11:06:22 -0400

    Starting to work on the ppu pipline

--- src/lcd.c
+++ src/lcd.c
@@ -3,35 +3,48 @@
 #include "./include/lcd.h"
 #include "./include/bus.h"
 
-#define LCDC 0xff40
-#define STAT 0xff41
-#define SCRLX 0xff42
-#define SCRLY 0xff43
-#define LY 0xff44
-#define LYC 0xff45
-#define DMA 0xff46
-#define BGP 0xff47
-#define OBP0 0xff48
-#define OBP1 0xff49
-#define WY 0xff4a
-#define WX 0xff4b
+#define LCDC 0x0
+#define STAT 0x1
+#define SCRLX 0x2
+#define SCRLY 0x3
+#define LY 0x4
+#define LYC 0x5
+#define DMA 0x6
+#define BGP 0x7
+#define OBP0 0x8
+#define OBP1 0x9
+#define WY 0xa
+#define WX 0xb
 
 static bool dmaTransfer = false;
 static uint8_t upperAddr;
 static uint8_t lowerAddr;
-static uint32_t clock = 0;
+static uint32_t dmaClock = 0;
+static uint32_t lcdClock = 0;
 
 static uint8_t lcdRegs[] = {
     0x91, 0x81, 0x00, 0x00, 0x91, 0x00, 0xff, 0xfc, 0xff, 0xff, 0x00, 0x00
 };
 
 void incLCDTimer(uint32_t cycles) {
-    clock += cycles;
-    while (clock > 4 && dmaTransfer) {
+    dmaClock += cycles;
+    lcdClock += cycles;
+    while (dmaClock > 3 && dmaTransfer) {
         busWrite8((0xfe00 | lowerAddr), busRead8((upperAddr << 8) | lowerAddr));
         lowerAddr++;
-        clock -= 4;
+        dmaClock -= 4;
         dmaTransfer = lowerAddr < 0xa0;
+    }
+
+    switch (lcdRegs[STAT] & 0x3) {
+	case 0:
+	    break;
+	case 1:
+	    break;
+	case 2:
+	    break;
+	case 3:
+	    break;
     }
 }
 
@@ -40,11 +53,11 @@ uint8_t readLCD(uint16_t addr) {
 }
 
 void writeLCD(uint16_t addr, uint8_t val) {
-    if (addr == DMA) {
+    if (addr == (DMA + 0xff40)) {
         dmaTransfer = true;
         upperAddr = val;
         lowerAddr = 0;
-        clock = 0;
+        dmaClock = 0;
     }
     lcdRegs[addr - 0xff40] = val;
 }
