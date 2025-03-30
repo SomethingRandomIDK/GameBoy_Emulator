ae5052dd6b70470032ba9ab3cfa8d1428aec83a5
mehul <mehtomar@gmail.com>
2025-03-30 02:33:27 -0400

    Redid the LCD register accesses
    Now when writing to different registers in the LCD has proper
    protections

--- src/lcd.c
+++ src/lcd.c
@@ -112,12 +112,49 @@ uint8_t readLCD(uint16_t addr) {
 }
 
 void writeLCD(uint16_t addr, uint8_t val) {
-    if (addr == (DMA + 0xff40)) {
-        dmaTransfer = true;
-        upperAddr = val;
-        lowerAddr = 0;
-        dmaClock = 0;
-    }
-    lcdRegs[addr - 0xff40] = val;
+    /* if (addr == (DMA + 0xff40)) { */
+    /*     dmaTransfer = true; */
+    /*     upperAddr = val; */
+    /*     lowerAddr = 0; */
+    /*     dmaClock = 0; */
+    /* } */
+    /* lcdRegs[addr - 0xff40] = val; */
+    switch(addr - 0xff40) {
+	case DMA:
+	    dmaTransfer = true;
+	    upperAddr = val;
+	    lowerAddr = 0;
+	    dmaClock = 0;
+	    break;
+	case STAT:
+	    if (lcdRegs[LCDC] & 0x80) {
+		if (((val & 0x08) ^ (lcdRegs[STAT] & 0x08)) && (lcdRegs[STAT] & 0x3) == 0) {
+		    raiseInterrupt(LCD);
+		}
+		if (((val & 0x10) ^ (lcdRegs[STAT] & 0x10)) && (lcdRegs[STAT] & 0x3) == 1) {
+		    raiseInterrupt(LCD);
+		}
+		if (((val & 0x20) ^ (lcdRegs[STAT] & 0x20)) && (lcdRegs[STAT] & 0x3) == 2) {
+		    raiseInterrupt(LCD);
+		}
+		if ((val & 0x40) ^ (lcdRegs[STAT] & 0x40)) {
+		    checkLy();
+		}
+	    }
+
+	    lcdRegs[STAT] = (val & 0x78) | (lcdRegs[STAT] & 0x7);
+	    break;
+	case LYC:
+	    lcdRegs[LYC] = val;
+	    if (lcdRegs[LCDC] & 0x80) {
+		checkLy();
+	    }
+	    break;
+	case LY:
+	    break;
+	default:
+	    lcdRegs[addr - 0xff40] = val;
+	    break;
+    } 
 }
 
