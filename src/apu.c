4c73e94eb3802020f35ac96e215d070abf11941d
mehul <mehtomar@gmail.com>
2025-07-28 05:03:21 -0400

    Added Resets and Fixed Some Bugs
    Added turning the APU on and off
    
    Fixed bugs for APU with write only registers
    Fixed bug with the Period overflowing and getting stuck in an infinite
    loop

--- src/apu.c
+++ src/apu.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "./include/apu.h"
 
 #define FR_64_HZ 65535
@@ -41,22 +43,22 @@ uint8_t soundRegs[0x30] = {
 #define CH_1_DIR (soundRegs[0x00] & 0x08)
 #define CH_1_STEP (soundRegs[0x00] & 0x07)
 #define CH_1_DUTY (soundRegs[0x01] >> 6)
-#define CH_1_LEN (soundRegs[0x01] & 0x3f)
 #define CH_1_VOL (soundRegs[0x02] >> 4)
 #define CH_1_ENV (soundRegs[0x02] & 0x8)
 #define CH_1_SWEEP (soundRegs[0x02] & 0x7)
-#define CH_1_PERIOD (soundRegs[0x03] | ((soundRegs[0x04] & 0x7) << 8))
 #define CH_1_LEN_EN (soundRegs[0x04] & 0x40)
 #define CH_1_TRIG (soundRegs[0x04] & 0x80)
 
-uint16_t ch1CurPeriod = 0x7ff;
-uint16_t ch1CurPeriodVal = 0x7ff;
+uint16_t ch1Period = 0x0;
+uint16_t ch1CurPeriod = 0x0;
+uint16_t ch1CurPeriodVal = 0x0;
 uint8_t ch1VolReg = 0xf3;
 uint8_t ch1Vol = 0xf;
 uint8_t ch1DutyIdx = 0;
 uint32_t ch1VolClock = 0;
 uint8_t ch1VolTimer = 0;
-uint8_t ch1LenTimer = 0x3f;
+uint8_t ch1Len = 0;
+uint8_t ch1LenTimer = 0x0;
 uint32_t ch1LenClock = 0;
 uint8_t ch1CurPace = 0;
 uint32_t ch1SweepClock = 0;
@@ -66,8 +68,32 @@ uint8_t ch1SweepTimer = 0;
 #define CH_1_CUR_ENV (ch1VolReg & 0x8)
 #define CH_1_CUR_SWEEP (ch1VolReg & 0x7)
 
+static void ch1Reset() {
+    // Reseting internal registers
+    ch1Period = 0x0;
+    ch1CurPeriod = 0x0;
+    ch1CurPeriodVal = 0x0;
+    ch1VolReg = 0x0;
+    ch1Vol = 0x0;
+    ch1DutyIdx = 0;
+    ch1VolClock = 0;
+    ch1VolTimer = 0;
+    ch1Len = 0x0;
+    ch1LenTimer = 0x0;
+    ch1LenClock = 0;
+    ch1CurPace = 0;
+    ch1SweepClock = 0;
+    ch1SweepTimer = 0;
+
+    // Reseting actual registers
+    soundRegs[0x00] &= 0x80;
+    soundRegs[0x01] &= 0x3f;
+    soundRegs[0x02] = 0;
+    soundRegs[0x04] &= 0xbf;
+}
+
 static uint16_t ch1CalcNewPeriod() {
-    uint16_t curPeriod = CH_1_PERIOD;
+    uint16_t curPeriod = ch1Period;
     uint16_t deltaVal = curPeriod >> CH_1_STEP;
 
     if (CH_1_DIR) {
@@ -84,7 +110,7 @@ static uint16_t ch1CalcNewPeriod() {
     // This assumes that if there is an underflow it would also be greater than
     // 0x7ff, since it is a unsigned number
     if (curPeriod > 0x7ff) {
-        curPeriod = CH_1_PERIOD;
+        curPeriod = ch1Period;
     }
 
     return curPeriod;
@@ -103,9 +129,9 @@ static void ch1Tick(uint32_t cycles) {
 
             // This means that a sample is over so the period can refresh from 
             // the APU registers
-            ch1CurPeriod = CH_1_PERIOD;
+            ch1CurPeriod = ch1Period;
         }
-        ch1CurPeriodVal = ch1CurPeriod + (ch1CurPeriodVal - 0x1000);
+        ch1CurPeriodVal = ch1CurPeriod;
     }
 
     if (CH_1_CUR_SWEEP) {
@@ -149,12 +175,7 @@ static void ch1Tick(uint32_t cycles) {
             ch1SweepTimer++;
             if (ch1SweepTimer >= ch1CurPace) {
                 ch1SweepTimer = 0;
-                uint16_t newPeriod = ch1CalcNewPeriod();
-
-                // Setting the new Period
-                soundRegs[0x03] = (newPeriod & 0xf);
-                soundRegs[0x04] &= 0xf8;
-                soundRegs[0x04] |= ((newPeriod >> 8) & 0x7);
+                ch1Period = ch1CalcNewPeriod();
             }
         }
     }
@@ -162,7 +183,6 @@ static void ch1Tick(uint32_t cycles) {
 
 // Channel 2 Functions and variables
 #define CH_2_DUTY (soundRegs[0x06] >> 6)
-#define CH_2_LEN (soundRegs[0x06] & 0x3f)
 #define CH_2_VOL (soundRegs[0x07] >> 4)
 #define CH_2_ENV (soundRegs[0x07] & 0x8)
 #define CH_2_SWEEP (soundRegs[0x07] & 0x7)
@@ -170,6 +190,7 @@ static void ch1Tick(uint32_t cycles) {
 #define CH_2_LEN_EN (soundRegs[0x09] & 0x40)
 #define CH_2_TRIG (soundRegs[0x09] & 0x80)
 
+uint16_t ch2Period = 0x7ff;
 uint16_t ch2CurPeriod = 0x7ff;
 uint16_t ch2CurPeriodVal = 0x7ff;
 uint8_t ch2VolReg = 0x00;
@@ -177,12 +198,33 @@ uint8_t ch2Vol = 0x0;
 uint8_t ch2DutyIdx = 0;
 uint32_t ch2VolClock = 0;
 uint8_t ch2VolTimer = 0;
+uint8_t ch2Len = 0x3f;
 uint8_t ch2LenTimer = 0x3f;
 uint32_t ch2LenClock = 0;
 
 #define CH_2_CUR_VOL (ch2VolReg >> 4)
 #define CH_2_CUR_ENV (ch2VolReg & 0x8)
 #define CH_2_CUR_SWEEP (ch2VolReg & 0x7)
+
+static void ch2Reset() {
+    // Reseting internal registers
+    ch2Period = 0x0;
+    ch2CurPeriod = 0x0;
+    ch2CurPeriodVal = 0x0;
+    ch2VolReg = 0x00;
+    ch2Vol = 0x0;
+    ch2DutyIdx = 0;
+    ch2VolClock = 0;
+    ch2VolTimer = 0;
+    ch2Len = 0x0;
+    ch2LenTimer = 0x0;
+    ch2LenClock = 0;
+
+    // Reseting external registers
+    soundRegs[0x06] &= 0x3f;
+    soundRegs[0x07] = 0;
+    soundRegs[0x09] &= 0xbf;
+}
 
 static void ch2Tick(uint32_t cycles) {
     uint32_t mClocks = cycles >> 2;
@@ -197,9 +239,9 @@ static void ch2Tick(uint32_t cycles) {
 
             // This means that a sample is over so the period can refresh from 
             // the APU registers
-            ch2CurPeriod = CH_2_PERIOD;
+            ch2CurPeriod = ch2Period;
         }
-        ch2CurPeriodVal = ch2CurPeriod + (ch2CurPeriodVal - 0x1000);
+        ch2CurPeriodVal = ch2CurPeriod;
     }
 
     if (CH_2_CUR_SWEEP) {
@@ -239,15 +281,31 @@ static void ch2Tick(uint32_t cycles) {
 #define CH_3_DAC (soundRegs[0x0a] & 0x80)
 #define CH_3_LEN (soundRegs[0x0b])
 #define CH_3_OUT_LV ((soundRegs[0x0c] >> 5) & 0x3)
-#define CH_3_PERIOD (soundRegs[0x0d] | ((soundRegs[0x0e] & 0x07) << 8))
 #define CH_3_LEN_EN (soundRegs[0x0e] & 0x40)
 #define CH_3_TRIG (soundRegs[0x0e] & 0x80)
 
+uint16_t ch3Period = 0x7ff;
 uint16_t ch3CurPeriod = 0x7ff;
 uint16_t ch3CurPeriodVal = 0x7ff;
+uint16_t ch3Len = 0xff;
 uint16_t ch3LenTimer = 0xff;
 uint32_t ch3LenClock = 0;
 uint8_t waveRamIdx = 0;
+
+static void ch3Reset() {
+    // Reseting internal Registers
+    ch3Period = 0x0;
+    ch3CurPeriod = 0x0;
+    ch3CurPeriodVal = 0x0;
+    ch3Len = 0x0;
+    ch3LenTimer = 0x0;
+    ch3LenClock = 0;
+    waveRamIdx = 0;
+
+    soundRegs[0x0a] &= 0x7f;
+    soundRegs[0x0c] &= 0x9f;
+    soundRegs[0x0e] &= 0xbf;
+}
 
 static void ch3Tick(uint32_t cycles) {
     uint32_t dotClock = cycles >> 1;
@@ -260,9 +318,9 @@ static void ch3Tick(uint32_t cycles) {
         if (waveRamIdx > 0x1f) {
             waveRamIdx = 0;
 
-            ch3CurPeriod = CH_3_PERIOD;
+            ch3CurPeriod = ch3Period;
         }
-        ch3CurPeriodVal = ch3CurPeriod + (ch3CurPeriodVal - 0x1000);
+        ch3CurPeriodVal = ch3CurPeriod;
     }
 
     if (CH_3_LEN_EN) {
@@ -279,7 +337,6 @@ static void ch3Tick(uint32_t cycles) {
 }
 
 // Channel 4 Functions and variables
-#define CH_4_LEN (soundRegs[0x10] & 0x3f)
 #define CH_4_VOL (soundRegs[0x11] >> 0x04)
 #define CH_4_ENV (soundRegs[0x11] & 0x08)
 #define CH_4_SWEEP (soundRegs[0x11] & 0x07)
@@ -293,6 +350,7 @@ uint8_t ch4VolReg = 0x00;
 uint8_t ch4Vol = 0;
 uint8_t ch4VolTimer = 0;
 uint32_t ch4VolClock = 0;
+uint8_t ch4Len = 0x3f;
 uint8_t ch4LenTimer = 0x3f;
 uint32_t ch4LenClock = 0;
 uint32_t ch4Clock = 0;
@@ -303,6 +361,23 @@ uint16_t ch4LSFR = 0;
 #define CH_4_CUR_VOL (ch4VolReg >> 4)
 #define CH_4_CUR_ENV (ch4VolReg & 0x8)
 #define CH_4_CUR_SWEEP (ch4VolReg & 0x7)
+
+static void ch4Reset() {
+    ch4VolReg = 0x00;
+    ch4Vol = 0;
+    ch4VolTimer = 0;
+    ch4VolClock = 0;
+    ch4Len = 0x0;
+    ch4LenTimer = 0x0;
+    ch4LenClock = 0;
+    ch4Clock = 0;
+    ch4FreqCycles = 0;
+    ch4LSFR = 0;
+
+    soundRegs[0x11] = 0;
+    soundRegs[0x12] = 0;
+    soundRegs[0x13] &= 0xbf;
+}
 
 static void ch4Tick(uint32_t cycles) {
     ch4Clock += cycles;
@@ -362,11 +437,42 @@ static void ch4Tick(uint32_t cycles) {
     }
 }
 
+#define AUD_ON (soundRegs[0x16] & 0x80)
+#define CH4_ON (soundRegs[0x16] & 0x08)
+#define CH3_ON (soundRegs[0x16] & 0x04)
+#define CH2_ON (soundRegs[0x16] & 0x02)
+#define CH1_ON (soundRegs[0x16] & 0x01)
+
+#define CH4_L_ON (soundRegs[0x15] & 0x80)
+#define CH3_L_ON (soundRegs[0x15] & 0x40)
+#define CH2_L_ON (soundRegs[0x15] & 0x20)
+#define CH1_L_ON (soundRegs[0x15] & 0x10)
+
+#define CH4_R_ON (soundRegs[0x15] & 0x08)
+#define CH3_R_ON (soundRegs[0x15] & 0x04)
+#define CH2_R_ON (soundRegs[0x15] & 0x02)
+#define CH1_R_ON (soundRegs[0x15] & 0x01)
+
+// Don't know what VIN values are supposed to do, so I'm just ignoring them for
+// now
+#define L_VOL ((soundRegs[0x14] >> 4) & 0x7)
+#define R_VOL (soundRegs[0x14] & 0x7)
+
 void incApuTimer(uint32_t cycles) {
-    ch1Tick(cycles);
-    ch2Tick(cycles);
-    ch3Tick(cycles);
-    ch4Tick(cycles);
+    if (!AUD_ON) return;
+
+    if (CH1_ON) {
+        ch1Tick(cycles);
+    }
+    if (CH2_ON) {
+        ch2Tick(cycles);
+    }
+    if (CH3_ON) {
+        ch3Tick(cycles);
+    }
+    if (CH4_ON) {
+        ch4Tick(cycles);
+    }
 }
 
 uint8_t readSound(uint16_t addr) {
@@ -377,13 +483,19 @@ void writeSound(uint16_t addr, uint8_t val) {
     uint16_t relAddr = addr - 0xff10;
     switch(relAddr) {
         case 0x16:
-            if ((soundRegs[0x16] & 0x80) > (val & 0x80)) {
+            if ((soundRegs[relAddr] & 0x80) > (val & 0x80)) {
                 // TODO Turing the APU off
-            } else if ((soundRegs[0x16] & 0x80) < (val & 0x80)) {
-                // TODO Turing the APU on
-            } else {
-                soundRegs[0x16] = (val & 0xf0) | (soundRegs[0x16] & 0x0f);
+                
+                soundRegs[0x16] &= 0x70;
+                soundRegs[0x15] = 0;
+                soundRegs[0x14] = 0;
+
+                ch1Reset();
+                ch2Reset();
+                ch3Reset();
+                ch4Reset();
             }
+            soundRegs[0x16] = (val & 0x80) | (soundRegs[0x16] & 0x7f);
             break;
         default:
             soundRegs[relAddr] = val;
